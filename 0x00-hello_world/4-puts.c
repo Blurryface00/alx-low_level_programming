dd57a2931099760fc36874e1708af990c0c6a6ab
blurryface00 <bigjayzy@gmail.com>
2023-08-11 01:58:41 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 
 /**
@@ -9,14 +8,11 @@
  *
  *    * Return: Always 0 (Success)
  *
- *     */
-
+ *    
+ */
 int main(void)
-
 {
-
-	        puts("\"Programming is like building a multilingual puzzle");
+	       puts("\"Programming is like building a multilingual puzzle");
 
 		        return (0);
-
 }
